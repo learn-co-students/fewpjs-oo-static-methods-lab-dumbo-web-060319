8ef9d9fc47986639a55625e0cdb5c0109d2a1d52
Sean Padden <you@example.com>
2019-07-28 11:47:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = []
+    let str = string.split(' ')
+    
+    str.forEach(word => {
+      if (!exceptions.includes(word)) {
+        arr.push(this.capitalize(word))
+      } else {
+        arr.push(word)
+      }
+    });
+    str = arr.join(' ')
+    return this.capitalize(str)
+  }
+
 }
